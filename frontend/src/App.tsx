3f97f05ea8e7298856aa5a5b9267b98d45baadf7
Pavan Vamsi <168455353+pavanvamsi1@users.noreply.github.com>
2026-03-02 11:39:46 +0530

    Refactor App component to use React Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,21 @@
-import { AuthProvider } from "./context/AuthContext";
-import AppRoutes from "./routes/AppRoutes";
-
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../src/pages/Home/Home";
+import Login from "../src/pages/Auth/Login";
+import Pricing from "../src/pages/Pricing/Pricing";
+import Team from "../src/pages/Team/Team";
+const App = () => {
   return (
-    <AuthProvider>
-      <AppRoutes />
-    </AuthProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/team" element={<Team />} />
+
+        <Route path="*" element={<h1>404 Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
