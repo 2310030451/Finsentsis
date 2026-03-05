a340f13e8e80b5a8b685a0fc885db6f6b5308573
Pavan Vamsi <168455353+pavanvamsi1@users.noreply.github.com>
2026-03-05 11:05:17 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthProvider";
 
 import Home from "./pages/Home/Home";
 import Login from "./pages/Auth/Login";
@@ -8,44 +9,46 @@ import Team from "./pages/Team/Team";
 
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 
-// Temporary dashboard components (replace later)
+// Temporary dashboard components
 const AdminDashboard = () => <div>Admin Dashboard</div>;
 const EmployeeDashboard = () => <div>Employee Dashboard</div>;
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Public Routes */}
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/Requestademo" element={<Requestademo />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/team" element={<Team />} />
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/Requestademo" element={<Requestademo />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/team" element={<Team />} />
 
-        {/* Protected Routes */}
-        <Route
-          path="/admin/dashboard"
-          element={
-            <ProtectedRoute allowedRoles={["ADMIN"]}>
-              <AdminDashboard />
-            </ProtectedRoute>
-          }
-        />
+          {/* Protected Routes */}
+          <Route
+            path="/admin/dashboard"
+            element={
+              <ProtectedRoute allowedRoles={["ADMIN"]}>
+                <AdminDashboard />
+              </ProtectedRoute>
+            }
+          />
 
-        <Route
-          path="/employee/dashboard"
-          element={
-            <ProtectedRoute allowedRoles={["EMPLOYEE"]}>
-              <EmployeeDashboard />
-            </ProtectedRoute>
-          }
-        />
+          <Route
+            path="/employee/dashboard"
+            element={
+              <ProtectedRoute allowedRoles={["EMPLOYEE"]}>
+                <EmployeeDashboard />
+              </ProtectedRoute>
+            }
+          />
 
-        {/* 404 */}
-        <Route path="*" element={<h1>404 Not Found</h1>} />
-      </Routes>
-    </BrowserRouter>
+          {/* 404 */}
+          <Route path="*" element={<h1>404 Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
