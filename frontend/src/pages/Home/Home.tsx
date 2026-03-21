685a15ff5229a5b9d3c86ba0dc23fe195fcab023
Pavan Vamsi <168455353+pavanvamsi1@users.noreply.github.com>
2026-03-21 17:34:06 +0530

    Update Home.tsx

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,5 +1,14 @@
+import Navbar from "../../components/Home/Navbar"; // adjust path if needed
+
 const Home = () => {
-  return <h1>Home Page 🚀</h1>;
+  return (
+    <>
+      <Navbar />
+      <h1 className="text-white text-3xl mt-32 text-center">
+        Home Page 🚀
+      </h1>
+    </>
+  );
 };
 
 export default Home;
